16614d10516ff4802244c688f0f1f1e5eafb46de
Anthony Restaino <anthonyrestaino11@gmail.com>
2015-10-18 21:51:19 -0400

    Initial Library Commit

---
+++ library/consumer-proguard-rules.pro
@@ -0,0 +1,9 @@
+# Remove all logging from production code
+-assumenosideeffects class android.util.Log {
+    public static boolean isLoggable(java.lang.String, int);
+    public static int v(...);
+    public static int i(...);
+    public static int w(...);
+    public static int d(...);
+    public static int e(...);
+}
---
+++ sample/src/main/java/com/anthonycr/sample/MainActivity.java
@@ -0,0 +1,165 @@
+package com.anthonycr.sample;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anthonycr.grant.PermissionsManager;
+import com.anthonycr.grant.PermissionsResultAction;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/test.txt";
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Requesting all the permissions in the manifest
+        PermissionsManager.getInstance().requestAllManifestPermissionsIfNecessary(this, new PermissionsResultAction() {
+            @Override
+            public void onGranted() {
+                Toast.makeText(MainActivity.this, R.string.message_granted, Toast.LENGTH_SHORT).show();
+                writeToStorage();
+                readFromStorage();
+            }
+
+            @Override
+            public void onDenied(String permission) {
+                String message = String.format(Locale.getDefault(), getString(R.string.message_denied), permission);
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button writeStorage = (Button) findViewById(R.id.button_write_storage);
+        Button readStorage = (Button) findViewById(R.id.button_read_storage);
+
+        this.textView = (TextView) findViewById(R.id.text);
+
+        writeStorage.setOnClickListener(this);
+        readStorage.setOnClickListener(this);
+    }
+
+    /**
+     * Requires Permission: Manifest.permission.WRITE_EXTERNAL_STORAGE
+     */
+    private void writeToStorage() {
+        File file = new File(PATH);
+        String test = "Hello, World!";
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file);
+            outputStream.write(test.getBytes());
+            this.textView.setText(String.format(Locale.getDefault(), getString(R.string.text_write), test));
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to write to storage", e);
+            this.textView.setText(R.string.text_failure_write);
+        } finally {
+            close(outputStream);
+        }
+    }
+
+    /**
+     * Requires Permission: Manifest.permission.READ_EXTERNAL_STORAGE
+     */
+    private void readFromStorage() {
+        File file = new File(PATH);
+        BufferedReader inputStream = null;
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream(file);
+            inputStream = new BufferedReader(new InputStreamReader(input));
+            String test = inputStream.readLine();
+            this.textView.setText(String.format(Locale.getDefault(), getString(R.string.text_read), test));
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to read from storage", e);
+            this.textView.setText(R.string.text_failure_read);
+        } finally {
+            close(input);
+            close(inputStream);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_write_storage:
+                PermissionsManager.getInstance().requestPermissionsIfNecessaryForResult(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, new PermissionsResultAction() {
+
+                            @Override
+                            public void onGranted() {
+                                writeToStorage();
+                            }
+
+                            @Override
+                            public void onDenied(String permission) {
+                                String message = String.format(Locale.getDefault(), getString(R.string.message_denied), permission);
+                                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                break;
+            case R.id.button_read_storage:
+                PermissionsManager.getInstance().requestPermissionsIfNecessaryForResult(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, new PermissionsResultAction() {
+
+                            @Override
+                            public void onGranted() {
+                                readFromStorage();
+                            }
+
+                            @Override
+                            public void onDenied(String permission) {
+                                String message = String.format(Locale.getDefault(), getString(R.string.message_denied), permission);
+                                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                break;
+        }
+    }
+
+    private static void close(@Nullable Closeable closeable) {
+        if (closeable == null) {return;}
+        try {
+            closeable.close();
+        } catch (IOException ignored) {}
+    }
+}
+
+
