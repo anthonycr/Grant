88bbfe3faf71e9d312f95016496932dbe919a6fd
anthony restaino <anthonyrestaino11@gmail.com>
2017-01-22 17:27:02 -0500

    Update to target latest android version

--- library/src/main/java/com/anthonycr/grant/PermissionsManager.java
+++ library/src/main/java/com/anthonycr/grant/PermissionsManager.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2017 Anthony C. Restaino
+ * <p/>
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.anthonycr.grant;
 
 import android.Manifest;
@@ -147,7 +167,7 @@ public class PermissionsManager {
     @SuppressWarnings("unused")
     public synchronized boolean hasPermission(@Nullable Context context, @NonNull String permission) {
         return context != null && (ActivityCompat.checkSelfPermission(context, permission)
-                == PackageManager.PERMISSION_GRANTED || !mPermissions.contains(permission));
+            == PackageManager.PERMISSION_GRANTED || !mPermissions.contains(permission));
     }
 
     /**
@@ -322,7 +342,7 @@ public class PermissionsManager {
                 if (!mPermissions.contains(perm)) {
                     action.onResult(perm, Permissions.NOT_FOUND);
                 } else if (ActivityCompat.checkSelfPermission(activity, perm)
-                        != PackageManager.PERMISSION_GRANTED) {
+                    != PackageManager.PERMISSION_GRANTED) {
                     action.onResult(perm, Permissions.DENIED);
                 } else {
                     action.onResult(perm, Permissions.GRANTED);
